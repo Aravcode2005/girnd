1fe370fd80cb9f8411922ab14f84c26a6a7b91bd
aarav <aravlead@gmail.com>
2025-09-01 12:18:26 +0530

    initial frontend app

---
+++ frontenddev/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './css/index.css';
+import App from './App';
+
+const a=false;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    { a ? <App/>:<h1>Hello form js in jsx</h1>}
+    <App />
+  </React.StrictMode>
+);
+
+
