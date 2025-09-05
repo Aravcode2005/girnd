fc8e172477033481d07cf46276a087be164a6447
aarav <aravlead@gmail.com>
2025-09-05 12:36:08 +0530

    commit

--- frontenddev/src/App.js
+++ frontenddev/src/App.js
@@ -1,44 +1,41 @@
-import {useState} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
-import ImageCard from './components/Imagecard';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Imagecard from "./components/Imagecard";
 
-const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY;
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 const App = () => {
- const [word, setWord] = useState('');
- const[images,setImages]=useState([])//Empty array at the start
+  const [word, setWord] = useState("");
+  const [images, setImages] = useState([]); //Empty array at the start
 
- const handleSearchSubmit = (e) => {
-  e.preventDefault(); 
-  console.log(word); 
-  fetch(` https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+    fetch(
+      ` https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages([{...data,title:word}, ...images]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-  .then((res)=> res.json())
-  .then((data)=>{
-   
-    setImages([data,...images]);
-  })
-  .catch((err)=>{
- 
-   console.log(err);
-  
-  })
-
-  setWord('');
-
-}
-console.log(process.env);
-
+    setWord("");
+  };
+  console.log(process.env);
 
   return (
     <div>
       <Header title="Images Gallery"/>
-      <Search 
-        word={word} 
-        setWord={setWord} 
-        handlesubmit={handleSearchSubmit} // Make sure this matches
-      />
+      <Search
+        word={word}
+        setWord={setWord}
+        handlesubmit={handleSearchSubmit}
+      ></Search>
+     {!!images.length  && <Imagecard image ={images[0]}/>}
     </div>
   );
 };
--- frontenddev/src/components/Imagecard.js
+++ frontenddev/src/components/Imagecard.js
@@ -1,37 +1,16 @@
-import React from 'react';
-
-import {Card,Button} from 'react-bootstrap';
-const ImageCard=()=>{
-return(
-<Card style={{width:'18rem'}}>
-
-<Card.Img variant="top" src="holder.js/100px180"/>
-
-<Card.Body>
-<Card.Title>
-    Card Title
-</Card.Title>
-
-<Card.Text>
-
-</Card.Text>
-<Button variant ="primary">
-
-
-</Button>
-
-
-</Card.Body>
-
-
-
-</Card>
-
-
-)
-
-
+import React from "react";
+import {Card, Button } from "react-bootstrap";
+const Imagecard = ({image}) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={image.urls.small} />
+      <Card.Body>
+        <Card.Title>{image.title}</Card.Title>
+        <Card.Text>{image.description||image.alt_description}</Card.Text>
+        <Button variant="primary">Delete</Button>
+      </Card.Body>
+    </Card>
+  );
 };
 
-
-export default ImageCard;
+export default Imagecard;
