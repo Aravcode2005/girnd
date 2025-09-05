dff57f38506a8060414782bb4ed0c3037c289585
aarav <aravlead@gmail.com>
2025-09-05 10:05:17 +0530

    save images in the state

--- frontenddev/src/App.js
+++ frontenddev/src/App.js
@@ -5,7 +5,8 @@ import Search from './components/Search';
 const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY;
 const App = () => {
  const [word, setWord] = useState('');
- 
+ const[images,setImages]=useState([])//Empty array at the start
+ console.log(images);
  const handleSearchSubmit = (e) => {
   e.preventDefault(); 
   console.log(word); 
@@ -13,8 +14,8 @@ const App = () => {
 
   .then((res)=> res.json())
   .then((data)=>{
-
-    console.log(data);
+   
+    setImages([data,...images]);
   })
   .catch((err)=>{
  
